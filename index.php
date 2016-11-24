58acfb1e2cca5df0a7d32f39e211aae8e3170a5a
Reshad007 <reshad_aziz@yahoo.com>
2016-11-24 06:18:45 -0800

    some updates

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 	<title>Shaunta's Boutique</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, 
+	 user - scalabble=no">
+	<script src="https:ajax.googleis.com/ajax/libs/jquery/2.1.2/jquery.min.js"></script>
 </head>
 
 <body>
