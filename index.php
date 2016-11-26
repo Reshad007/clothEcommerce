4fc0add76bba5089ba152492d66da1337f14e29e
Reshad007 <reshad_aziz@yahoo.com>
2016-11-25 21:41:10 -0800

    *Added menu bar and navigation bar   *

--- index.php
+++ index.php
@@ -12,10 +12,30 @@
 	 user - scalabble=no">
 	 <!-- Links Ajax javascript directory -->
 	<script src="https:ajax.googleis.com/ajax/libs/jquery/2.1.2/jquery.min.js"></script>
-	script
+	<!-- Links javascript bootstrap file-->
+	<script> src="js/bootstrap.min.js"</script>
 </head>
 
 <body>
-	
+	<!-- Navbar starts here -->
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<!-- Container starts here -->
+		<div class="container">
+			<!-- Adds home button to the main page -->
+			<a href="index.php" class="navbar-brand">Shaunta's Boutiqe</a>
+			<!-- Navigation bar starts here -->
+			<ul class="nav navbar-nav">
+				<!-- Drop down menue starts here -->
+				<li class="dropdown">
+					<!-- Dropdown menue button -->
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu <span class="caret"></span></a>
+				<!-- Drop down menue finishes here -->	
+				</li>
+			<!-- Navigation bar finishes here -->	
+			</ul>
+		</div>
+		<!-- container finishes here -->
+	</nav>
+	<!-- Navbar finishes here -->
 </body>
 </html>
