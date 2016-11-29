f1a740a367c2f8f6b1c08fa7a56944bca749ce3f
Reshad007 <reshad_aziz@yahoo.com>
2016-11-29 14:34:59 -0800

    added includes to index.php

--- index.php
+++ index.php
@@ -1,60 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<!-- Title of the page -->
-	<title>Shaunta's Boutique</title>
-	<!-- Links bootsrtrap css file -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<!-- Links my own css file -->
-	<link rel="stylesheet" href="css/main.css">
-	<!-- Makes it dynamic for all kind of screen resolutions -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, 
-	 user - scalabble=no">
-	 <!-- Links Ajax javascript directory -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<!-- Links javascript bootstrap file-->
-	<script src="js/bootstrap.min.js"></script>
-</head>
-
-<body>
-
-<!-- Navbar starts here -->
-<nav class="navbar navbar-default navbar-fixed-top">
-	<!-- Container starts here -->
-	<div class="container">
-		<!-- Adds home button to the main page -->
-		<a href="index.php" class="navbar-brand">Shaunta's Boutiqe</a>
-		<!-- Navigation bar starts here -->
-		<ul class="nav navbar-nav">
-			<!-- Drop down menue starts here -->
-			<li class="dropdown">
-				<!-- Dropdown menue button --> 								   <!-- Sign after "menue" -->
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu<span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-					<li><a href="#">Shirts</a></li>
-					<li><a href="#">Pants</a></li>
-					<li><a href="#">Shoes</a></li>
-					<li><a href="#">Accessories</a></li>
-				</ul>
-			<!-- Drop down menue finishes here -->	
-			</li>
-		<!-- Navigation bar finishes here -->	
-		</ul>
-	</div>
-	<!-- container finishes here -->
-</nav>
-<!-- Navbar finishes here -->
+<?php 
+	include 'includes/head.php'; 
+	include 'includes/navigation.php';
+	include 'includes/headerfull.php';
+?>
 
 
-<!-- Header -->
-<div id="headerWrapper">
-	<div id="back-flower"></div>
-	<div id="logotext"></div>
-	<div id="fore-flower"></div>
-</div>
-
-<!-- Content -->
-<div class="container-fluid">
 	<!-- Left Side Bar -->
 	<div class="col-md-2">Left Side Bar</div>
 
