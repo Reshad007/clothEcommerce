5a9b99e7ffd601a52be5aa115a64f6cc739ca21a
Reshad007 <reshad_aziz@yahoo.com>
2016-11-30 23:28:08 -0800

    Adjusted connection to database through online

--- core/init.php
+++ core/init.php
@@ -5,11 +5,10 @@
 <?php 
     //                    host ip    username  pw  name of db 
 	/*$db = mysqli_connect('127.0.0.1','root',   '', 'clothEcommerce');*/
-	$host_name  = "db646482344.db.1and1.com";
-    $database   = "db646482344";
-    $user_name  = "dbo646482344";
-    $password   = "ufobed88ufobed88";
-
+	$host_name  = "db658757653.db.1and1.com";
+    $database   = "db658757653";
+    $user_name  = "dbo658757653";
+    $password   = "Ufobed88ufobed88:";
 
     $db = mysqli_connect($host_name, $user_name, $password, $database);
 
