3156e5bcc5a3bf3fa9a2704f9bee5b74154745fe
Reshad007 <reshad_aziz@yahoo.com>
2016-11-30 22:58:03 -0800

    Adjusted connection to database through online

--- core/init.php
+++ core/init.php
@@ -1,6 +1,18 @@
+
+
+
+
 <?php 
     //                    host ip    username  pw  name of db 
-	$db = mysqli_connect('127.0.0.1','root',   '', 'clothEcommerce');
+	/*$db = mysqli_connect('127.0.0.1','root',   '', 'clothEcommerce');*/
+	$host_name  = "db646482344.db.1and1.com";
+    $database   = "db646482344";
+    $user_name  = "dbo646482344";
+    $password   = "ufobed88ufobed88";
+
+
+    $db = mysqli_connect($host_name, $user_name, $password, $database);
+
 	if (mysqli_connect_errno()) {
 		echo "Data base connection failed with following errors: ".mysqli_connect_error();
 		die();
